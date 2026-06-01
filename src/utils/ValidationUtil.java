6b1ebd18c6dc095196f07717cf81d01eeac371c1
Nihalmuhd <nihal.muhd.mn@gmail.com>
2026-06-01 22:56:22 +0530

    test updated

--- src/utils/ValidationUtil.java
+++ src/utils/ValidationUtil.java
@@ -4,7 +4,7 @@ public class ValidationUtil {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Nihal");
+System.out.println("Anzana psycho");
 	}
 
 }
